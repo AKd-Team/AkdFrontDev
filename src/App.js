90567f95d25f6fc5b9cd4f2ca97694f5e39a54e8
Marian George <50915367+georgeMarian1999@users.noreply.github.com>
2020-10-31 18:40:57 +0200

    First Set-Up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Button from '@material-ui/core/Button';
 function App() {
   return (
     <div className="App">
@@ -17,6 +17,9 @@ function App() {
         >
           Learn React
         </a>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
       </header>
     </div>
   );
