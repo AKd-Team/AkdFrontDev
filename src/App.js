1c5ec1587762ee2827d5caf58d359a9c826f7677
Marian George <50915367+georgeMarian1999@users.noreply.github.com>
2020-11-03 20:38:11 +0200

    Am adaugat directoarele in care o sa fie ecranele respectiv dashboard urile. In App.js am adaugat putin conditional render de care vorbeam iar crtPage care este state va fi modificat in functie de response ul care vine de la server. Eu am sa lucrez la studentdashboard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import StudentDashboard from "./screens/StudentDashboard/StudentDashboard";
+import TeacherDashboard from "./screens/TeacherDashboard/TeacherDashboard";
+import AdminDashboard from "./screens/AdminDashboard/AdminDashboard";
+import LoginScreen from "./screens/LoginScreen/LoginScreen";
 const App = () =>{
-    return <StudentDashboard />;
+    const [crtPage,setCrtPage]=useState('studentdash');
+    if(crtPage==='loginscreen'){
+        return <LoginScreen />
+    }
+    if(crtPage==='studentdash'){
+        return <StudentDashboard />;
+    }
+    if(crtPage=='teacherdash'){
+        return <TeacherDashboard />
+    }
+    if(crtPage==='admindash'){
+        return <AdminDashboard />
+    }
 };
 
 export default App;
