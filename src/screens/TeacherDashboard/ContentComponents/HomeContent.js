b7a5af01a1cedfd927511466d878848d77760946
Marian George <50915367+georgeMarian1999@users.noreply.github.com>
2020-11-16 21:10:21 +0200

    Request and redirect for all users

--- src/screens/TeacherDashboard/ContentComponents/HomeContent.js
+++ src/screens/TeacherDashboard/ContentComponents/HomeContent.js
@@ -4,9 +4,10 @@ import {useHistory} from "react-router";
 const HomeContent = () =>{
     const history=useHistory();
     const User=JSON.parse(localStorage.getItem("user"));
+    var HomeText;
     if(User!=null){
         if(User.Type==="teacher"){
-
+            HomeText=`This is home for ${User.username} with first name ${User.firstname} and last name ${User.lastname}`;
         }
         else {
             history.push(`/${User.Type}dash/${User.Type}`);
@@ -17,7 +18,7 @@ const HomeContent = () =>{
     }
     return(
         <div>
-            <h1>This is home</h1>
+            <h1>{HomeText}</h1>
         </div>
     );
 };
