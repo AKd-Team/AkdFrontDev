7ad243369a5b6a7d33ddc0be48b1374a6590bcc8
Marian George <50915367+georgeMarian1999@users.noreply.github.com>
2021-01-11 15:46:59 +0200

    Home Content ptr admin si profesor. Statistici implementate cu API. Programare examen functional

--- src/screens/TeacherDashboard/ContentComponents/HomeContent.js
+++ src/screens/TeacherDashboard/ContentComponents/HomeContent.js
@@ -1,27 +1,174 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {useHistory} from "react-router";
+import {Divider, Header, Icon, Image, Segment,Grid} from 'semantic-ui-react'
+import {makeStyles} from "@material-ui/core/styles";
+import {motion} from "framer-motion";
+import {useAnimation} from "framer-motion";
 import Fade from 'react-reveal';
+import {Link} from 'react-router-dom';
+const styles = makeStyles((theme) => ({
+    root:{
+        //justifyContent: 'center',
+        alignContent: 'center',
+        margin:'4%',
+    },
+    photo:{
+        marginLeft: '3%',
+        marginRight: '3%',
+    },
+}));
 const HomeContent = () =>{
     const history=useHistory();
+    const style=styles();
     const User=JSON.parse(localStorage.getItem("user"));
     var HomeText;
-    if(User!=null){
-        if(User.tipUtilizator==="profesor"){
-            HomeText=`This is home for ${User.username} with first name ${User.prenume} and last name ${User.prenume}`;
+    useEffect(()=>{
+        if(User!=null){
+            if(User.tipUtilizator==="profesor"){
+               // HomeText=`This is home for ${User.username} with first name ${User.prenume} and last name ${User.prenume}`;
+            }
+            else {
+                history.push(`/${User.tipUtilizator}dash/${User.tipUtilizator}`);
+            }
         }
-        else {
-            history.push(`/${User.tipUtilizator}dash/${User.tipUtilizator}`);
+        else{
+            history.push("/");
         }
-    }
-    else{
-        history.push("/");
-    }
+    },)
+    const header = useAnimation()
+    header.start({
+        x:"4%",
+        transition: { duration: 2 },
+    })
     return(
-        <div>
-            <Fade bottom>
-            <h1>{HomeText}</h1>
+        <Segment className={style.root}>
+            <motion.div
+                animate={header}
+            >
+                <Header as='h2'>
+                    <Icon name='university'/>
+                    <Header.Content>Universitatea Babes-Bolyai</Header.Content>
+                </Header>
+            </motion.div>
+            <Fade left big>
+                <Divider/>
             </Fade>
-        </div>
+            <motion.div className={style.photo}>
+                <Fade>
+                    <Image
+                        src="https://files.primariaclujnapoca.ro/universitate/MG_0437_Universitatea-Babes-Bolyai.jpg"
+                        fluid
+                        href='https://www.ubbcluj.ro/ro/'
+                        target='_blank'
+                        rounded
+                    />
+                </Fade>
+            </motion.div>
+            <Fade right big>
+                <Divider/>
+            </Fade>
+            <motion.div
+                animate={header}
+            >
+                <Header as='h2'>
+                    <Icon name='archive'/>
+                    <Header.Content>Resurse Profesori</Header.Content>
+                </Header>
+            </motion.div>
+            <Fade right big>
+                <Divider/>
+            </Fade>
+            <Grid textAlign='center' columns={3} divided>
+                <Grid.Row>
+                    <Grid.Column>
+                        <Fade left>
+                            <h3>Facilitati IT</h3>
+                        </Fade>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Fade top>
+                            <h3>Invatamant</h3>
+                        </Fade>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Fade right>
+                            <h3>Resurse interne</h3>
+                        </Fade>
+                    </Grid.Column>
+                </Grid.Row>
+                <Divider/>
+                <Grid.Row>
+                    <Grid.Column>
+                        <Fade left>
+                            <a href='https://login.microsoftonline.com/' target="_blank">
+                                <h4>Portal Microsoft</h4>
+                            </a>
+                        </Fade>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Fade top>
+                            <a href='https://infocercetare.ubbcluj.ro/' target="_blank">
+                                <h4>Managementul activităţii academice/ştiinţifice UBB</h4>
+                            </a>
+                        </Fade>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Fade right>
+                            <a href='https://www.ubbcluj.ro/ro/infoubb/posturi_vacante/posturi_cercetare' target="_blank">
+                                <h4>Posturi vacante în cadrul echipelor proiectelor de cercetare</h4>
+                            </a>
+                        </Fade>
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                    <Grid.Column>
+                        <Fade left>
+                            <a href='https://outlook.office365.com/' target="_blank">
+                                <h4>Outlook Mail</h4>
+                            </a>
+                        </Fade>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Fade top>
+                            <a href='http://cms.ubbcluj.ro/Account/Login' target="_blank">
+                                <h4>CMSUBB / DISPOZIȚIA RECTORULUI</h4>
+                            </a>
+                        </Fade>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Fade right>
+                            <a href='https://www.ubbcluj.ro/ro/staff/resurse_umane.php' target="_blank">
+                                <h4> Evaluarea performanțelor profesionale individuale ale personalului didactic auxiliar și nedidactic</h4>
+                            </a>
+                        </Fade>
+                    </Grid.Column>
+                </Grid.Row>
+
+                <Grid.Row>
+                    <Grid.Column>
+                        <Fade left>
+                            <a href='https://www.scs.ubbcluj.ro/webmail/' target="_blank">
+                                <h4>SCS Mail</h4>
+                            </a>
+                        </Fade>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Fade top>
+                            <a href='https://manageasist.ubbcluj.ro/' target="_blank">
+                                <h4>Managementul financiar al proiectelor instituționale</h4>
+                            </a>
+                        </Fade>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Fade right>
+                            <a href='https://www.ubbcluj.ro/ro/staff/recrutare_selectie/' target="_blank">
+                                <h4>Recrutarea, selecția și promovarea personalului didactic auxiliar și nedidactic</h4>
+                            </a>
+                        </Fade>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        </Segment>
     );
 };
 
--- src/screens/TeacherDashboard/ContentComponents/Statistici.js
+++ src/screens/TeacherDashboard/ContentComponents/Statistici.js
@@ -1,23 +1,220 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {useHistory} from "react-router";
-
+import Paper from '@material-ui/core/Paper';
+import {
+    Chart,
+    ArgumentAxis,
+    ValueAxis,
+    BarSeries,
+    Legend,
+} from '@devexpress/dx-react-chart-material-ui';
+import { ValueScale, Animation } from '@devexpress/dx-react-chart';
+import {makeStyles, withStyles} from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import InputLabel from "@material-ui/core/InputLabel";
+import {CircularProgress, Select} from "@material-ui/core";
+import Input from "@material-ui/core/Input";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import axios from "axios";
+const useStyles = makeStyles((theme) => ({
+    root:{
+        justifyContent:'center',
+        alignItems: 'center',
+        width: '100%',
+    },
+    chart:{
+        marginLeft:50,
+        marginRight:50,
+    },
+    select:{
+        marginLeft:theme.spacing(20),
+        marginRight:50,
+        marginTop:40,
+    },
+    formControl: {
+        marginRight: theme.spacing(7),
+        minWidth: 150,
+    },
+    title:{
+        marginTop:'5%',
+        marginLeft:'5%',
+    }
+}));
 const Statistici = () =>{
     const history=useHistory();
+    const classes=useStyles();
+    const [materii,setMaterii]=useState([]);
     const User=JSON.parse(localStorage.getItem("user"));
-    if(User!=null){
-        if(User.tipUtilizator==="profesor"){
+    useEffect(()=>{
+        if(User!=null){
+            if(User.tipUtilizator==="profesor"){
 
+            }
+            else {
+                history.push(`/${User.tipUtilizator}dash/${User.tipUtilizator}`);
+            }
+        }
+        else{
+            history.push("/");
+        }
+    },[])
+    const data={
+        "null":[
+            { nota: "1", Nrstudenti: 0 },
+            { nota: "2", Nrstudenti: 0 },
+            { nota: "3", Nrstudenti: 0 },
+            { nota: "4", Nrstudenti: 0 },
+            { nota: "5", Nrstudenti: 0 },
+            { nota: "6", Nrstudenti:0 },
+            { nota: "7", Nrstudenti: 0 },
+            { nota: "8", Nrstudenti: 0 },
+            { nota: "9", Nrstudenti: 0},
+            { nota: "10",Nrstudenti: 0 },
+        ]
+    };
+    const onChangeMateria = (e)=>{
+        setMateria(e.target.value);
+    }
+    const [stats,setStats]=useState(data["null"]);
+    const [materia,setMateria]=useState('null');
+    const [loading,setLoading]=useState(false);
+    const timer = React.useRef();
+    useEffect(()=>{
+        console.log(materia)
+        if(materia==='null'){
+            setStats(data[materia]);
         }
         else {
-            history.push(`/${User.tipUtilizator}dash/${User.tipUtilizator}`);
+            setLoading(true);
+            timer.current = window.setTimeout(async () => {
+                await axios.get("http://localhost:4000/profesor/materii/statistici/"+`${materia}`, {
+                    headers: {
+                        'Authorization': `token ${User.token}`
+                    }
+                })
+                    .then((response) => {
+                        let data = [];
+                        response.data.forEach((stats) => {
+                            if(stats.Nrstudenti===undefined)
+                                 data.push({
+                                    nota: stats.nota,
+                                    Nrstudenti: 0,
+                                })
+                            else data.push({
+                                nota: stats.nota,
+                                Nrstudenti: stats.Nrstudenti,
+                            })
+                        });
+                        console.log(data);
+                        setStats(data);
+                        setLoading(false);
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            }, 1000);
         }
-    }
-    else{
-        history.push("/");
-    }
+        return () => {
+            clearTimeout(timer.current);
+        };
+    },[materia])
+    useEffect(()=>{
+        setLoading(true);
+        axios.get("http://localhost:4000/profesor/"+`${User.id}`+"/materii", {
+            headers: {
+                'Authorization': `token ${User.token}`
+            }
+        })
+            .then((response) => {
+                let listaMaterii = [];
+                response.data.forEach((materie) => {
+                    listaMaterii.push({
+                        key: materie.idMaterie,
+                        text: materie.nume,
+                        value: materie.idMaterie,
+                    })
+                });
+                console.log(listaMaterii);
+                let max;
+                if(listaMaterii.length!==0)
+                    max = listaMaterii.reduce((prev, current) => (prev.key > current.key) ? prev : current);
+                else max=1;
+                listaMaterii.push({
+                    key: max,
+                    text: 'Not selected',
+                    value: 'null',
+                })
+                setLoading(false);
+                setMaterii(listaMaterii);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+    },[])
     return(
-        <div>
-            <h1>This is Statistici</h1>
+        <div className={classes.root}>
+            <Grid container spacing={3}>
+                <Grid item xs={3}>
+                    <div className={classes.title}>
+                        <h1>Statistici note studenti pentru materia selectata</h1>
+                    </div>
+                </Grid>
+                <Grid item xs={3}>
+                    <div className={classes.select}>
+                        <FormControl className={classes.formControl}>
+                            <InputLabel id="demo-simple-select-label">Materia</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                value={materia}
+                                onChange={onChangeMateria}
+                                input={<Input />}
+                            >
+                                {materii.map((materie) => (
+                                    <MenuItem key={materie.key} value={materie.value}>
+                                        {materie.text}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    </div>
+                </Grid>
+                <Grid item xs={4}>
+                    <div className={classes.select}>
+                        {loading && <CircularProgress />}
+                    </div>
+                </Grid>
+                <Grid item xs={12}>
+                    <Paper className={classes.chart}>
+
+                        <Chart
+                            data={stats}
+                        >
+
+                            <ValueScale name="Nrstudenti" />
+
+
+                            <ArgumentAxis />
+                            <ValueAxis scaleName="Nrstudenti" showGrid={false} showLine showTicks />
+
+                            <BarSeries
+                                name="Studentii care au avut nota respectiva"
+                                valueField="Nrstudenti"
+                                argumentField="nota"
+                                scaleName="Nrstudenti"
+                            />
+
+                            <Animation />
+
+                            <Legend />
+
+                        </Chart>
+                    </Paper>
+                </Grid>
+
+            </Grid>
         </div>
     );
 };
